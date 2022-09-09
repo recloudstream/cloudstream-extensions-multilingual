c6e3bbc7869f6b95cdd9489c6bcbc0ac5881c3b2
Eddy <kingkama976@gmail.com>
2022-09-09 22:12:39 +0200

    resolve conflict

--- VostfreeProvider/src/main/kotlin/com/lagradost/VostfreeProvider.kt
+++ VostfreeProvider/src/main/kotlin/com/lagradost/VostfreeProvider.kt
@@ -9,12 +9,7 @@ import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 import org.jsoup.nodes.Element
 
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.utils.AppUtils.toJson
-import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
-import org.jsoup.nodes.Element
+
 
 
 class VostfreeProvider : MainAPI() {
