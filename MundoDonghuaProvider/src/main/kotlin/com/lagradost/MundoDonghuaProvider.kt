589c14b22e875a8d8633d22262a5b50ebe819ceb
Diego Sánchez <zenit4d@gmail.com>
2022-12-12 00:06:09 -0300

    Repair last chapters bug

--- MundoDonghuaProvider/src/main/kotlin/com/lagradost/MundoDonghuaProvider.kt
+++ MundoDonghuaProvider/src/main/kotlin/com/lagradost/MundoDonghuaProvider.kt
@@ -36,9 +36,15 @@ class MundoDonghuaProvider : MainAPI() {
                     val title = it.selectFirst("h5")?.text() ?: ""
                     val poster = it.selectFirst(".fit-1 img")?.attr("src")
                     val epRegex = Regex("(\\/(\\d+)\$)")
-                    val url = it.selectFirst("a")?.attr("href")?.replace(epRegex,"")?.replace("/ver/","/donghua/")
+                    var url = it.selectFirst("a")?.attr("href")?.replace(epRegex,"")?.replace("/ver/","/donghua/")
+                    var urllast = url?.last()
                     val epnumRegex = Regex("((\\d+)$)")
                     val epNum = epnumRegex.find(title)?.value?.toIntOrNull()
+                    val epNumLenght = epNum.toString()!!.length
+                    if (urllast.toString() == "="){
+                        val numbersfinal = 61 + epNumLenght + 1
+                        url = url?.dropLast(numbersfinal)
+                    }
                     val dubstat = if (title.contains("Latino") || title.contains("Castellano")) DubStatus.Dubbed else DubStatus.Subbed
                     newAnimeSearchResponse(title.replace(Regex("Episodio|(\\d+)"),"").trim(), fixUrl(url ?: "")) {
                         this.posterUrl = fixUrl(poster ?: "")
