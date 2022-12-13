b17f625b920b7cd39f4f51b76995aaf5b6c79808
Diego Sánchez <zenit4d@gmail.com>
2022-12-12 23:58:33 -0300

    Final commit with syntax errors fixed

--- MundoDonghuaProvider/src/main/kotlin/com/lagradost/MundoDonghuaProvider.kt
+++ MundoDonghuaProvider/src/main/kotlin/com/lagradost/MundoDonghuaProvider.kt
@@ -111,7 +111,7 @@ class MundoDonghuaProvider : MainAPI() {
             } else {
                 null
             }
-        }?.reversed() ?: emptyMap()
+        }?.reversed() ?: emptyMap<Episode>()
         val episodes = doc.select("ul.donghua-list a").map {
             val name = it.selectFirst(".fs-16")?.text()
             val link = it.attr("href")
